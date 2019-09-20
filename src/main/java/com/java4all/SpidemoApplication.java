0cdf581ba592af19ec290e44e1505b02335a65d5
ITyunqing <1186355422@qq.com>
2019-09-20 08:57:59 +0800

    optimize

--- src/main/java/com/java4all/SpidemoApplication.java
+++ src/main/java/com/java4all/SpidemoApplication.java
@@ -3,6 +3,9 @@ package com.java4all;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author IT云清
+ */
 @SpringBootApplication
 public class SpidemoApplication {
 	public static void main(String[] args) {
